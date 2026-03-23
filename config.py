8f061dcb77429c3deb2fefb6a0debcb5b14a5e5b
varvvaov-stack <varvvaov@gmail.com>
2026-03-24 01:07:06 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 TOKEN = "8631920472:AAH-g62R96z8JjBrRI7ea-OizxePXkvhSxk"   # твой токен
-CHANNEL_ID = "@dfsfdsfs432234"                             # твой канал
+CHANNEL_ID = "@gokerbatls"                             # твой канал
 ADMIN_IDS = [7847231769]                                   # твой Telegram ID
